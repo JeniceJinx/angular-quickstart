a9fa1da0adf6bf27aef798be0ad301574d69170f
Jinx <JeniceJinx@users.noreply.github.com>
2022-10-15 19:57:12 -0400

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['../demo-styling.css']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-quickstart';
+  title = 'Portfolio';
 }
