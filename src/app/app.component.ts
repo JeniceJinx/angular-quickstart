b06c7521abe6224a463b754490b541b49daff107
Jinx <JeniceJinx@users.noreply.github.com>
2022-10-15 19:57:51 -0400

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators, NgForm } from '@angular/forms'
+import { ContactService } from './contact.service';
+
+
 
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-contact-form',
+  templateUrl: './contacts.component.html',
+  styleUrls: ['./contacts.component.css']
 })
-export class AppComponent {
+export class ContactFormComponent implements OnInit {
   title = 'Portfolio';
+  submitted = false;
+  emailForm!: FormGroup;
+ 
+  constructor(private formBuilder: FormBuilder) {}
+ 
+  ngOnInit() {
+    this.emailForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+ 
+  onSubmit() {
+    this.submitted = true;
+ 
+    // stop the process here if form is invalid
+    if (this.emailForm.invalid) {
+      return;
+    }
+ 
+    alert('SUCCESS!!');
+  }
 }
