4a57dd18156c423e537db25b00fd0f7657e984d1
Jinx <JeniceJinx@users.noreply.github.com>
2022-10-15 19:59:41 -0400

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,11 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { Component } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { FooterComponent } from './sharepage/footer/footer.component';
-import { HomeComponent } from './pages/home/home.component';
-import { ProjectsComponent } from './pages/projects/projects.component';
-import { AboutComponent } from './pages/about/about.component';
-import { AppRoutingModule } from './app-routing.module';
-import { RouterModule } from '@angular/router';
-import { NavComponent } from './sharepage/nav/nav.component';
-import { ContactFormComponent } from './pages/contacts/contacts.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatCommonModule } from '@angular/material/core';
-import {MatCardModule} from '@angular/material/card';
-import { IndprojComponent } from './pages/indproj/indproj.component';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    NavComponent,
-    FooterComponent,
-    HomeComponent,
-    ProjectsComponent,
-    AboutComponent,
-    ContactFormComponent,
-    IndprojComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    RouterModule,
-    BrowserAnimationsModule,
-    MatSliderModule,
-    MatCardModule,
-    MatCommonModule,
-    HttpClientModule,
-    ReactiveFormsModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppModule { }
+export class AppComponent {
+  title = 'Portfolio';
+}
