fa6191383779aee18d4e47b35f232a56f039450c
Jinx <JeniceJinx@users.noreply.github.com>
2022-10-15 19:55:40 -0400

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './pages/home/home.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactFormComponent } from './pages/contacts/contacts.component';
+import { IndprojComponent } from './pages/indproj/indproj.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'projects', component:ProjectsComponent},
+  {path:'projects/:id', component:IndprojComponent},
+  {path:'about', component:AboutComponent},
+  {path:'contacts', component:ContactFormComponent}
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ]
 })
 export class AppRoutingModule { }
